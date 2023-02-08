1405ded5819d579dc9adc544e307bad9cd6d0ce1
Marta M <marmacan.upv@gmail.com>
2023-02-08 13:50:39 +0100

    Creada BD database_links

--- database/db.sql
+++ database/db.sql
@@ -3,4 +3,21 @@ CREATE DATABASE database_links CHARSET utf8mb4;
 USE database_links;
 
 -- USERS TABLE
-CREATE TABLE users
+CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+-- links TABLE
+CREATE TABLE links(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description text,
+    user_id INT UNSIGNED,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
