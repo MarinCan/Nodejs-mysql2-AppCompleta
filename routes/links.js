b459cd2045964ebd01c6467f6f436bb018fd22a0
Marta M <marmacan.upv@gmail.com>
2023-02-15 10:38:32 +0100

    Implementado boton borrar y funcion timeago

--- routes/links.js
+++ routes/links.js
@@ -7,8 +7,13 @@ const pool = require('../database')
 router.get('/', async (req, res, next) => {
 
   //con esto podemos saber si esta conectando bien:
-  const [result] = await pool.query("SELECT 1+1")
-  res.json(result)
+  // const [result] = await pool.query("SELECT 1+1")
+
+  const [ links ] = await pool.query("SELECT * FROM links")
+
+  console.log(links)
+  // res.send('Lista de links here!')
+  res.render('links/list', { links })
 
   // res.send('LINKS !! ');
 });
@@ -18,8 +23,28 @@ router.get('/add', (req, res) => {
   res.render('links/add')
 })
 
-router.post('/add', (req, res) => {
-  res.send('Recibido')
+router.post('/add', async (req, res) => {
+  console.log(req.body)
+
+  const {title, url, description} = req.body
+  const new_link = {
+    title,
+    url,
+    description
+  }
+
+  // console.log(new_link)
+
+  await pool.query('INSERT INTO links SET ?', [new_link])
+  
+  res.redirect('/links')
+})
+
+router.get('/delete/:id', async (req, res) => {
+  // console.log(req.params.id)
+  const { id } = req.params
+  await pool.query('DELETE FROM links WHERE id = ?', [id])
+  res.redirect('/links')
 })
 
 module.exports = router;
