1f111255d38cd996fa693c748df4a26f24654178
Marta M <marmacan.upv@gmail.com>
2023-02-17 09:20:46 +0100

    Creada estrategia signin

--- routes/authentication.js
+++ routes/authentication.js
@@ -27,8 +27,23 @@ router.post('/signup', passport.authenticate('local.signup', {
   failureFlash: true
 }))
 
+router.get('/signin', (req, res) => {
+  res.render('auth/signin')
+})
+
+router.post('/signin', (req, res, next) => {
+  // console.log(req.body)
+  // res.send('Login correcto')
+  passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash: true
+  })(req, res, next)
+})
+
 router.get('/profile', (req, res) => {
-  res.send('Personal profile')
+  // res.send('Personal profile')
+  res.render('profile')
 })
 
 module.exports = router;
